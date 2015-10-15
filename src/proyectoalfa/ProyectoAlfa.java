d6d483faf976cb8b0c99b80de8fa15f7ae503367
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-10-15 08:55:27 +0200

    cambiamos base

--- src/proyectoalfa/ProyectoAlfa.java
+++ src/proyectoalfa/ProyectoAlfa.java
@@ -8,7 +8,7 @@ public class ProyectoAlfa {
     
     public static void main(String[] args) {
         
-    float base = 2.0f, area ; //Poño un f por ser de tipo flout, se nn saltame a double que e o principal
+    float base = 6.0f, area ; //Poño un f por ser de tipo flout, se nn saltame a double que e o principal
     
     float altura = (float) 3.0;// fago un cast, ponse float ou o tipo a convertir entre parentesis, se nn se pon a f.
     
