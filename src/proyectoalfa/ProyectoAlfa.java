c07e9bb721f73f7c69be97c508cfed2129afd166
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-10-15 09:19:28 +0200

    comentario

--- src/proyectoalfa/ProyectoAlfa.java
+++ src/proyectoalfa/ProyectoAlfa.java
@@ -11,7 +11,7 @@ public class ProyectoAlfa {
     float base = 6.0f, area ; //Poño un f por ser de tipo flout, se nn saltame a double que e o principal
     
     float altura = (float) 8.0;// fago un cast, ponse float ou o tipo a convertir entre parentesis, se nn se pon a f.
-    
+    //comentario
     area = base*altura/2;
     
     System.out.println("area = "+area);
